d159d33a146cbd1479d0b55363383eadd0d90a70
David Cramer <dcramer@gmail.com>
2011-01-21 13:50:20 -0800

    Added a basic option to post to twitter via the admin (draftish)

--- overseer/admin.py
+++ overseer/admin.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.contrib import admin
 
+from overseer import conf
 from overseer.models import Service, Event, EventUpdate
 
 class ServiceAdmin(admin.ModelAdmin):
@@ -7,15 +9,29 @@ class ServiceAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     prepopulated_fields = {'slug': ('name',)}
 
+class EventForm(forms.ModelForm):
+    if conf.TWITTER_ACCESS_TOKEN and conf.TWITTER_ACCESS_SECRET:
+        post_to_twitter = forms.BooleanField(required=False, label="Post an update to Twitter when I hit save", help_text="This will send a tweet with a brief summary, the permalink to the event (if BASE_URL is defined), and the hashtag of #status")
+
+    class Meta:
+        model = EventUpdate
+
 class EventUpdateInline(admin.StackedInline):
     model = EventUpdate
     extra = 1
 
 class EventAdmin(admin.ModelAdmin):
+    form = EventForm
     list_display = ('date_created', 'description', 'status', 'date_updated')
     search_fields = ('description', 'message')
     list_filter = ('services',)
     inlines = [EventUpdateInline]
+
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save()
+        if form.cleaned_data['post_to_twitter']:
+            for obj in instances:
+                obj.event.post_to_twitter(obj.get_message())
 
 class EventUpdateAdmin(admin.ModelAdmin):
     list_display = ('date_created', 'message', 'status', 'event')
