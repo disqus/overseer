cdfbdc2c924a9b5cb894dc4fbeaa7d8b0e91c84e
David Cramer <dcramer@gmail.com>
2011-02-02 17:35:56 -0800

    ReadTheDocs doesnt like our versioning

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, DISQUS'
 # built documents.
 #
 # The short X.Y version.
-version = __import__('Overseer').VERSION
+version = '0.1.2'
 # The full version, including alpha/beta/rc tags.
 release = version
 
