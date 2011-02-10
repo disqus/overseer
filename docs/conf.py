20749959155f4ccbc0db8c44066a99d64ffc3d5d
David Cramer <dcramer@gmail.com>
2011-02-09 16:40:57 -0800

    Bump docs version

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ copyright = u'2011, DISQUS'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1.2'
+version = '0.2.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
